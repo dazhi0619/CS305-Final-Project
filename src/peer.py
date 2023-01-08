b0dbb311e3c136fbcb7de88a0300b7f5230e6ce3
dazhi0619 <da.zhi0619@gmail.com>
2023-01-09 02:24:06 +0800

    task1 finished

--- src/peer.py
+++ src/peer.py
@@ -15,9 +15,12 @@ from util import simsocket
 # configurations
 BUF_SIZE = 1400
 HEADER_LEN = struct.calcsize("HBBHHII")
-CHUNK_DATA_SIZE = 512 * 1024
 MAX_PAYLOAD = 1024
+CHUNK_DATA_SIZE = 512 * 1024
 MAGIC = 52305
+HASHLEN = 40
+SENDPKT = ">HBBHHII"
+RECVPKT = ">HBBHHII"
 
 # packet types
 WHOHAS = 0
@@ -35,6 +38,8 @@ RECV = 2
 
 
 receiving_chunks = {}
+# indicate whether the user have instructed to download
+download_instruction = False
 
 
 class Peer:
@@ -52,20 +57,22 @@ class Peer:
         self.sending = {}
         # [(team number, ip, port, state)], records the address of other peers
         self.peers = [(n, i, p, DISCONNECTED) for n, i, p in configuration.peers]
-        # {checksum : data}, records the data that this peer already has
-        self.haschunks = config.haschunks
+        # {checksum(str) : data}, records the data that this peer already has
+        self.haschunks = {
+            bytes(x, "ascii"): config.haschunks[x] for x in config.haschunks
+        }
         # {team number : checksum}, record the chunks of the peers that have initiated handshake
         self.peerchunks = {}
         self.team = configuration.identity
         self.maxconn = configuration.max_conn
 
     def constr_packet(self, packet_type, seq, ack, data: bytes):
-        # |2byte magic|1byte type |1byte team|
+        # |2byte magic|1byte team|1byte type|
         # |2byte  header len  |2byte pkt len |
         # |      4byte  seq                  |
         # |      4byte  ack                  |
         header = struct.pack(
-            "HBBHHII",
+            SENDPKT,
             socket.htons(MAGIC),
             self.team,
             packet_type,
@@ -78,13 +85,16 @@ class Peer:
         return pkt
 
     def constr_whohas(self, download_hash):
-        byte = b""
+        data = b""
         for d in download_hash:
-            byte += bytes(d, "ascii")
-        return self.constr_packet(WHOHAS, 0, 0, byte)
+            data += d
+        return self.constr_packet(WHOHAS, 0, 0, data)
 
     def constr_ihave(self, pkt_list):
-        return self.constr_packet(IHAVE, 0, 0, bytes(pkt_list))
+        data = b""
+        for p in pkt_list:
+            data += p
+        return self.constr_packet(IHAVE, 0, 0, data)
 
     def constr_denied(self):
         return self.constr_packet(DENIED, 0, 0, b"")
@@ -99,13 +109,17 @@ class Peer:
             lines = cf.readlines()
             for line in lines:
                 _, datahash_str = line.strip().split(" ")
+                datahash_str = bytes(datahash_str, encoding="ascii")
                 receiving_chunks.update({datahash_str: bytes()})
-                self.demand.append(datahash_str)
+                if (
+                    datahash_str not in self.demand
+                    and datahash_str not in self.haschunks
+                ):
+                    self.demand.append(datahash_str)
 
-            # hex_str to bytes
             datahash = b""
             for s in self.demand:
-                datahash += bytes.fromhex(s)
+                datahash += s
             download_hash = download_hash + datahash
 
         self.broadcast(sock)
@@ -113,114 +127,126 @@ class Peer:
     def process_inbound_udp(self, sock):
         # Receive pkt
         pkt, from_addr = sock.recvfrom(BUF_SIZE)
-        _, Team, Type, hlen, plen, Seq, Ack = struct.unpack("HBBHHII", pkt[:HEADER_LEN])
+        _, Team, Type, hlen, plen, Seq, Ack = struct.unpack(RECVPKT, pkt[:HEADER_LEN])
+        hlen = socket.ntohs(hlen)
+        plen = socket.ntohs(plen)
         dlen = plen - hlen
         data = pkt[hlen:]
+        seq_num = socket.ntohl(Seq)
+        ack_num = socket.ntohl(Ack)
+        print(f"received pkt: {Team} {Type} {hlen} {dlen} {seq_num} {ack_num}")
         if Type == WHOHAS:
             # received an WHOHAS pkt
             # see what chunk the sender has
             whohas_chunk_hash_list = []
             i = 0
             while i < dlen:
-                whohas_chunk_hash = data[i : i + 20]
-                i += 20
-                # bytes to hex_str
-                chunkhash_str = bytes.hex(whohas_chunk_hash)
-                whohas_chunk_hash_list.append(chunkhash_str)
+                whohas_chunk_hash = data[i : i + HASHLEN]
+                # print(f"whohas_chunk_hash = {whohas_chunk_hash}")
+                i += HASHLEN
+                if whohas_chunk_hash not in whohas_chunk_hash_list:
+                    whohas_chunk_hash_list.append(whohas_chunk_hash)
 
-            for s in whohas_chunk_hash_list:
-                print(f"whohas: {s}")
-            print(f"has: {list(config.haschunks.keys())}")
-            ihave_pkt_list = [
-                h for h in whohas_chunk_hash_list if h in config.haschunks
-            ]
+            print(f"has: {list(self.haschunks.keys())}")
+            ihave_pkt_list = [h for h in whohas_chunk_hash_list if h in self.haschunks]
+            print(f"ihave_pkt_list = {ihave_pkt_list}")
             if len(self.sending) < MAX_PAYLOAD and ihave_pkt_list:
                 # send back IHAVE pkt
                 ihave_pkt = self.constr_ihave(ihave_pkt_list)
                 sock.sendto(ihave_pkt, from_addr)
 
                 # change the state to HANDSHAKE
-                idx = next(i for i, v in self.peers if v[0] == Team)
-                self.peers[idx] = (self.peers[idx][0:2], HANDSHAKE)
+                idx = next(i for i, v in enumerate(self.peers) if v[0] == str(Team))
+                self.peers[idx] = (*self.peers[idx][0:3], HANDSHAKE)
             elif len(self.sending) >= self.maxconn and ihave_pkt_list:
                 denied_header = self.constr_denied()
                 sock.sendto(denied_header, from_addr)
-        elif Type == 1:
+        elif Type == IHAVE:
             # received an IHAVE pkt
             # see what chunk the sender has
             get_chunk_hash_list = []
             i = 0
             while i < dlen:
-                get_chunk_hash = data[i : i + 20]
-                i += 20
-                chunkhash_str = bytes.hex(get_chunk_hash)
-                get_chunk_hash_list.append(chunkhash_str)
+                get_chunk_hash = data[i : i + HASHLEN]
+                i += HASHLEN
+                get_chunk_hash_list.append(get_chunk_hash)
             self.peerchunks.update({Team: get_chunk_hash_list})
+            print(f"Team {Team} has: {get_chunk_hash_list}")
 
             # change the state to HANDSHAKE
-            idx = next(i for i, v in self.peers if v[0] == Team)
-            self.peers[idx] = (self.peers[idx][0:2], HANDSHAKE)
-        elif Type == 2:
+            idx = next(i for i, v in enumerate(self.peers) if v[0] == str(Team))
+            self.peers[idx] = (*self.peers[idx][0:3], HANDSHAKE)
+        elif Type == GET:
             print(f"receive get from peer {Team}")
             # received a GET pkt
-            send_chunk_checksum = data[hlen : hlen + 20]
+            send_chunk_checksum = data[:HASHLEN]
+            print(f"send_chunk_checksum = {send_chunk_checksum}")
             self.sending.update({Team: send_chunk_checksum})
             chunk_data = self.haschunks[self.sending[Team]][:MAX_PAYLOAD]
 
             # send back DATA
             data_header = struct.pack(
-                "HBBHHII",
+                SENDPKT,
                 socket.htons(MAGIC),
                 self.team,
                 DATA,
                 socket.htons(HEADER_LEN),
-                socket.htons(HEADER_LEN),
+                socket.htons(HEADER_LEN + len(chunk_data)),
                 socket.htonl(1),
                 0,
             )
             sock.sendto(data_header + chunk_data, from_addr)
 
             # change the state to SEND
-            idx = next(i for i, v in self.peers if v[0] == Team)
-            self.peers[idx] = (self.peers[idx][0:2], SEND)
-        elif Type == 3:
+            idx = next(i for i, v in enumerate(self.peers) if v[0] == str(Team))
+            self.peers[idx] = (*self.peers[idx][0:3], SEND)
+        elif Type == DATA:
             # change the state to RECV
-            idx = next(i for i, v in self.peers if v[0] == Team)
-            self.peers[idx] = (self.peers[idx][0:2], RECV)
+            idx = next(i for i, v in enumerate(self.peers) if v[0] == str(Team))
+            self.peers[idx] = (*self.peers[idx][0:3], RECV)
             # received a DATA pkt
-            receiving_chunks[self.downloading[Team]] += data
+            h = self.downloading[Team]
+            receiving_chunks[h] = receiving_chunks[h] + data
 
             # send back ACK
             ack_pkt = struct.pack(
-                "HBBHHII",
+                SENDPKT,
                 socket.htons(MAGIC),
                 self.team,
                 ACK,
                 socket.htons(HEADER_LEN),
                 socket.htons(HEADER_LEN),
-                0,
-                Seq,
+                socket.htonl(ack_num),
+                socket.htonl(seq_num + 1),
             )
             sock.sendto(ack_pkt, from_addr)
 
             # see if finished
+            print(
+                f"In process_inbound_udp:DATA: len(receiving_chunks[h]) = {len(receiving_chunks[h])}"
+            )
             if len(receiving_chunks[self.downloading[Team]]) == CHUNK_DATA_SIZE:
                 # finished downloading this chunkdata!
                 # dump your received chunk to file in dict form using pickle
-                with open(self.ex_output_file, "wb") as wf:
-                    pickle.dump(receiving_chunks, wf)
+                if len(self.demand) == 0:
+                    with open(self.ex_output_file, "wb") as wf:
+                        pickle.dump(receiving_chunks, wf)
+                    # you need to print "GOT" when finished downloading all chunks in a DOWNLOAD file
+                    print(f"GOT {self.ex_output_file}")
 
                 # add to this peer's haschunk:
                 self.haschunks[self.downloading[Team]] = receiving_chunks[
                     self.downloading[Team]
                 ]
-
-                # you need to print "GOT" when finished downloading all chunks in a DOWNLOAD file
-                print(f"GOT {self.ex_output_file}")
+                if self.downloading[Team] in self.demand:
+                    self.demand.remove(self.downloading[Team])
 
                 # change the state to DISCONNECTED
-                idx = next(i for i, v in self.peers if v[0] == Team)
-                self.peers[idx] = (self.peers[idx][0:2], DISCONNECTED)
+                idx = next(i for i, v in enumerate(self.peers) if v[0] == str(Team))
+                self.peers[idx] = (*self.peers[idx][0:3], DISCONNECTED)
+
+                # initial another handshake session
+                self.initial_download(sock, chunkf, outf)
 
                 # # The following things are just for illustration, you do not need to print out in your design.
                 # sha1 = hashlib.sha1()
@@ -234,33 +260,32 @@ class Peer:
                 #     print("Congrats! You have completed the example!")
                 # else:
                 #     print("Example fails. Please check the example files carefully.")
-        elif Type == 4:
+        elif Type == ACK:
             # received an ACK pkt
-            ack_num = socket.ntohl(Ack)
-            if (ack_num) * MAX_PAYLOAD >= CHUNK_DATA_SIZE:
+            if (ack_num - 1) * MAX_PAYLOAD >= CHUNK_DATA_SIZE:
                 # finished
                 print(f"finished sending {self.sending[Team]}")
 
                 # change the state to DISCONNECTED
-                idx = next(i for i, v in self.peers if v[0] == Team)
-                self.peers[idx] = (self.peers[idx][0:2], DISCONNECTED)
+                idx = next(i for i, v in enumerate(self.peers) if v[0] == str(Team))
+                self.peers[idx] = (*self.peers[idx][0:3], DISCONNECTED)
             else:
-                left = (ack_num) * MAX_PAYLOAD
-                right = min((ack_num + 1) * MAX_PAYLOAD, CHUNK_DATA_SIZE)
+                left = (ack_num - 1) * MAX_PAYLOAD
+                right = min((ack_num) * MAX_PAYLOAD, CHUNK_DATA_SIZE)
                 next_data = self.haschunks[self.sending[Team]][left:right]
                 # send next data
                 data_header = struct.pack(
-                    "HBBHHII",
+                    SENDPKT,
                     socket.htons(MAGIC),
-                    35,
-                    3,
+                    self.team,
+                    DATA,
                     socket.htons(HEADER_LEN),
                     socket.htons(HEADER_LEN + len(next_data)),
-                    socket.htonl(ack_num + 1),
-                    0,
+                    socket.htonl(ack_num),
+                    socket.htonl(seq_num),
                 )
                 sock.sendto(data_header + next_data, from_addr)
-        elif Type == 5:
+        elif Type == DENIED:
             # received an DENIED pkt
             pass
         else:
@@ -268,10 +293,12 @@ class Peer:
             pass
 
     def process_user_input(self, sock):
+        global chunkf
+        global outf
         command, chunkf, outf = input().split(" ")
         if command == "DOWNLOAD":
+            globals()["download_instruction"] = True
             self.initial_download(sock, chunkf, outf)
-            self.get(sock)
         else:
             pass
 
@@ -284,19 +311,17 @@ class Peer:
     def get(self, sock):
         # randomly choose peers for desired chunks, send GET
         for d in self.demand:
-            print(f"{d} is in self.demand")
             for p in self.peerchunks:
-                print(f"{p} is in self.peerchunks")
-                if d in self.peerchunks[p]:
+                peer_idx = next(i for i, v in enumerate(self.peers) if v[0] == str(p))
+                if self.peers[peer_idx][3] == HANDSHAKE and d in self.peerchunks[p]:
                     # change the state to HANDSHAKE
-                    idx = next(i for i, v in self.peers if v[0] == p)
-                    self.peers[idx] = (self.peers[idx][0:2], HANDSHAKE)
+                    self.peers[peer_idx] = (*self.peers[peer_idx][0:3], RECV)
 
                     self.downloading.update({p: d})
                     self.peerchunks[p].remove(d)
                     self.demand.remove(d)
-                    get_pkt = self.constr_packet(GET, 0, 0, d)
-                    dest = next(x for x in self.peers if x[0] == p)
+                    get_pkt = self.constr_packet(GET, 0, 1, d)
+                    dest = next(x for x in self.peers if x[0] == str(p))
                     sock.sendto(get_pkt, (dest[1], int(dest[2])))
                     print(f"send get to {p} for {d}")
 
@@ -319,9 +344,9 @@ def peer_run(configuration):
                     peer.process_user_input(sock)
             else:
                 # No pkt nor input arrives during this period
+                if download_instruction:
+                    peer.get(sock)
                 pass
-                # peer.broadcast(sock)
-                # peer.get(sock)
     except KeyboardInterrupt:
         pass
     finally:
